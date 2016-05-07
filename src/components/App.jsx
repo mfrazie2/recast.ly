a8b0203f4bbb0f954ecbc4fa7448b363eb443f97
Mike Frazier <michael.s.frazier@gmail.com>
2016-05-07 14:09:34 -0500

    Update App to class component; update child components to handle click event on video titles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,32 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={ exampleVideoData[0] } />
+class App extends React.Component {  
+  constructor () {
+    super();
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+  
+  handleVideoClick (video) {
+    this.setState({
+      currentVideo: video
+    });
+  } 
+  
+  render () { 
+    return (
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={ this.state.currentVideo } />
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={ this.state.videos } listClick={this.handleVideoClick.bind(this)} />
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList videos={ exampleVideoData }/>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
