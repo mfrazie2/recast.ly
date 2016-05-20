872a0bb8f584ad1c5464fd6fcfef2248d515e18e
Mike Frazier <michael.s.frazier@gmail.com>
2016-05-20 15:16:42 -0500

    Remove comments from searchYouTube file

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,11 +1,4 @@
 var searchYouTube = (options, callback) => {
-//   $.ajax({
-//     method: 'GET',
-//     url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + options.query + '&type=video&maxResults=' + options.max + '&videoEmbeddable=true&key=' + options.key,
-//     success: callback,
-//     error: (err) => { console.error('There was an error!', err); callback(); }
-//   });
-// };
   $.get('https://www.googleapis.com/youtube/v3/search', {
     part: 'snippet',
     key: options.key,
